c73f1c8d0556bc205d277476777c6c7372ec5456
Erik de Jonge <erik@a8.nl>
2015-06-03 13:41:55 +0200

    Ludwig Mies van der Rohe: God is in the details. Wednesday 03 June 2015 (week:22 day:154), 13:41:54

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ standard_library.install_aliases()
 
 from setuptools import setup
 setup(name='vckube',
-      version='29',
+      version='30',
       description='Kubernetes Vagrant Provisioning and management script',
       url='https://github.com/erikdejonge/vckube',
       author='Erik de Jonge',
