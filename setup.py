1a7b2a1f459c2cfacbfed891b0941abbffba647a
Erik de Jonge <erik@a8.nl>
2015-06-16 13:18:28 +0200

    Charles de Montesquieu: True success, true happiness lies in freedom and fulfillment. Tuesday 16 June 2015 (week:24 day:167), 13:18:26

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ standard_library.install_aliases()
 
 from setuptools import setup
 setup(name='vckube',
-      version='32',
+      version='33',
       description='Kubernetes Vagrant Provisioning and management script',
       url='https://github.com/erikdejonge/vckube',
       author='Erik de Jonge',
