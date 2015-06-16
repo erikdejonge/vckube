88bdb46ef6c2b59b7b27c84ac8811d5c2985c14c
Erik de Jonge <erik@a8.nl>
2015-06-16 15:59:32 +0200

    Peter Drucker: Nothing that is Gods is obtainable by money. Tuesday 16 June 2015 (week:24 day:167), 15:59:30

--- setup.py
+++ setup.py
@@ -4,22 +4,16 @@ Profiler utility for python
 Active8 (04-03-15)
 license: GNU-GPL2
 """
-from __future__ import unicode_literals
-from __future__ import print_function
-from __future__ import division
-from __future__ import absolute_import
-from future import standard_library
-standard_library.install_aliases()
 
 from setuptools import setup
 setup(name='vckube',
-      version='34',
+      version='35',
       description='Kubernetes Vagrant Provisioning and management script',
       url='https://github.com/erikdejonge/vckube',
       author='Erik de Jonge',
       author_email='erik@a8.nl',
       license='GPL',
-      packages=[b'vckube'],
+      packages=['vckube'],
       zip_safe=True,
       #install_requires=['paramiko', 'python-vagrant', 'consoleprinter', 'cmdssh'],
       entry_points={
