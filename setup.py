cf306e7b81208a69657c926ee8e2842cf36b4e18
Erik de Jonge <erik@a8.nl>
2015-10-05 13:59:52 +0200

    Monday 05 October 2015 (week:40 day:278), 13:59:32

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ license: GNU-GPL2
 
 from setuptools import setup
 setup(name='vckube',
-      version='47',
+      version='48',
       description='Kubernetes Vagrant Provisioning and management script',
       url='https://github.com/erikdejonge/vckube',
       author='Erik de Jonge',
